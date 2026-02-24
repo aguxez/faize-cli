e1f4516fe60c94829352bede5e278aa9647dcb39
Miguel Diaz <github.com.hf06j@slmail.me>
2026-02-24 02:12:17 +0100

    🧹 vm: remove non-darwin build stubs

--- internal/vm/oauth_relay_stub.go
+++
@@ -1,6 +0,0 @@
-//go:build !darwin
-
-package vm
-
-func parseOAuthRedirect(rawURL string) (string, bool) { return "", false }
-func startOAuthRelay(done <-chan struct{}, bootstrapDir string, port string) error { return nil }
--- internal/vm/open_url_stub.go
+++
@@ -1,6 +0,0 @@
-//go:build !darwin
-
-package vm
-
-// watchOpenURL is a no-op on non-darwin platforms.
-func watchOpenURL(done <-chan struct{}, bootstrapDir string) {}
